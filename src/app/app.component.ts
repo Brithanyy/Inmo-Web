cbdb0680e64707d12f3983de263ead15315c6b3a
Brithanyy <109824084+Brithanyy@users.noreply.github.com>
2024-10-30 12:44:37 -0300

    instalando dependencias necesarias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { ContactComponent } from "./Pages/contact/contact.component";
 import { LoginComponent } from './Pages/management-login/management-login.component';
 
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, ContactComponent, LoginComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'proyecto-final';
