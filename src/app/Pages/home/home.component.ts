804f0b322e24ec7719f40d25620e7bc7e1938614
Rocio Ortiz <114239750+Rouse02@users.noreply.github.com>
2024-10-30 11:38:09 -0300

    Creación del header

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { PropertyCardComponent } from "../../Components/property-card/property-card.component";
 import { NavbarComponent } from "../../Components/navbar/navbar.component";
 import { FooterComponent } from "../../Components/footer/footer.component";
+import { HeaderComponent } from "../../Components/header/header.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PropertyCardComponent, NavbarComponent, FooterComponent],
+  imports: [PropertyCardComponent, NavbarComponent, FooterComponent, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
