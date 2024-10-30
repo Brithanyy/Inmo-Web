b9c9824b5d8a2977397328bddcb4099a5f585bf1
Brithanyy <109824084+Brithanyy@users.noreply.github.com>
2024-10-30 11:38:27 -0300

    creacion de la ruta login e implementacion del front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContactComponent } from "./Pages/contact/contact.component";
+import { LoginComponent } from './Pages/management-login/management-login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContactComponent],
+  imports: [RouterOutlet, ContactComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
