4a71bfea79f0a95eda3e86bb1693204fb0e6286d
Rocio Ortiz <114239750+Rouse02@users.noreply.github.com>
2024-11-07 20:39:14 -0300

    Desarrollo de la page home de gestión

--- src/app/Pages/management-home/management-home.component.ts
+++ src/app/Pages/management-home/management-home.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { UserService } from '../../Services/User/user.service';
+import { User } from '../../Models/User.model';
+import { NavbarComponent } from "../../Components/navbar/navbar.component";
+import { PropertiesComponent } from "../properties/properties.component";
+import { ListHousesComponent } from '../../Components/list-houses/list-houses.component';
+import { ListDepartamentsComponent } from '../../Components/list-departaments/list-departaments.component';
+import { ListLandsComponent } from '../../Components/list-lands/list-lands.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-management-home',
   standalone: true,
-  imports: [],
+  imports: [
+    NavbarComponent,
+    ListHousesComponent,
+    ListDepartamentsComponent,
+    ListLandsComponent
+],
   templateUrl: './management-home.component.html',
   styleUrl: './management-home.component.css'
 })
-export class ManagementHomeComponent {
+export class ManagementHomeComponent implements OnInit{
+
+  mensajeError? = '';
+  usuarios: User[] = [];
+  userBuffer?: User;
+  servicioUsuario = inject(UserService);
+  router = inject(Router);
+
+  ngOnInit(): void {
+    
+    this.obtenerUsuarioLogueado();
+  }
+
+  obtenerUsuarioLogueado() {
+
+    this.servicioUsuario.getAllUsers().subscribe({
+
+      next: (returnedUsers: User[]) => this.userBuffer = returnedUsers.find(user => user.estaLogueado === true),
+    
+      error: () => {
+        this.showErrorMessage("Error al obtener usuario logueado");
+      }
+    });
+  }
+
+  cerrarSesion() {
+    
+  }
+
+  private showErrorMessage(mensaje: string) {
+
+    this.mensajeError = mensaje;
+  
+    setTimeout(() => {
+      this.mensajeError = '';
+    }, 3000);
+  }
 
 }
