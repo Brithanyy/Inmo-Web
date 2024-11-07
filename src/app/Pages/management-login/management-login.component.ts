f58b09dbeb60c10fe0e4bb562a8a85f4c2b26763
Rocio Ortiz <114239750+Rouse02@users.noreply.github.com>
2024-11-07 19:53:00 -0300

    Creción del servicio Usuario, cambiamos el form del login por un formulario reactivo, se incluyeron validaciones

--- src/app/Pages/management-login/management-login.component.ts
+++ src/app/Pages/management-login/management-login.component.ts
@@ -1,31 +1,96 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from '../../Models/User.model';
+import { UserService } from '../../Services/User/user.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule],
   templateUrl: './management-login.component.html',
   styleUrl: './management-login.component.css'
 })
 export class LoginComponent {
-  logo = 'assets/IMG/LogoTipo.jpeg'
-  usuario = {
-    user: '', //Almacena el valor del nombre de usuario
-    password: '' // Almacena el valor de la contraseña
-  }
-  isPasswordVisible: boolean = false; // Controla la visibilidad de la contraseña
+
+  mensajeError: string = '';
+  isPasswordVisible: boolean = false;
+
+  servicioUsuario =  inject(UserService);
+  router = inject(Router);
+  logo = 'assets/IMG/LogoTipo.jpeg';
+  formBuiler = inject(FormBuilder);
+  formLogin = this.formBuiler.nonNullable.group({
+
+    userName: ['', Validators.required],
+    password: ['', [Validators.required,  Validators.minLength(8)]]
+  });
 
   togglePasswordVisibility() {
-    this.isPasswordVisible = !this.isPasswordVisible; // Alterna la visibilidad
+    this.isPasswordVisible = !this.isPasswordVisible;
   }
 
-  userAdmin = 'admin';
-  passAdmin = 'admin';
+  get userName() {
+    return this.formLogin.get('userName');
+  }
+
+  get password() {
+    return this.formLogin.get('password');
+  }
 
   login() {
-    if(this.usuario.user === this.userAdmin && this.usuario.password === this.passAdmin) alert("INICIANDO SESION");
+
+    if (this.formLogin.valid) {
+
+      this.mensajeError = '';
+
+      let usuarioBuffer: User = this.formLogin.getRawValue();
+
+      this.servicioUsuario.getUserByCredentials(usuarioBuffer.userName, usuarioBuffer.password).subscribe({
+
+        next: (returnedUser: User) => {
+
+          if (returnedUser) {
+
+            this.servicioUsuario.updateUserLoggedStatus(String(returnedUser.id), true).subscribe({
+
+              next: () => this.router.navigate(['/management-home']),
+
+              error: (returnedError) => {
+
+                this.mensajeError = "Error al actualizar el estado del usuario: " + returnedError.message;
+                this.showErrorMessage(this.mensajeError);
+              }
+            });
+          } 
+
+          else {
+            this.mensajeError = "Error las credenciales del usuario son inválidas";
+            this.showErrorMessage(this.mensajeError);
+          }
+        },
+        error: (returnedError) => {
+          this.mensajeError = "Error en el inicio de sesión";
+          this.showErrorMessage(this.mensajeError);
+        }
+      });
+    }
+    else {
+      this.mensajeError = "Por favor, complete todos los campos";
+      this.showErrorMessage(this.mensajeError);
+    }
   }
 
+  private showErrorMessage(mensaje: string) {
+
+    this.mensajeError = mensaje;
+  
+    setTimeout(() => {
+      this.mensajeError = '';
+    }, 3000);
+  }
 }
--- src/app/Services/User/user.service.ts
+++ src/app/Services/User/user.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../../Models/User.model';
+import { catchError, map, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,15 @@ import { Injectable } from '@angular/core';
 export class UserService {
 
   constructor() { }
+
+  peticionesHTTP = inject(HttpClient);
+  urlBase = 'http://localhost:3000/Usuario';
+
+  getUserByCredentials(username: string, password: string): Observable<User> {
+    return this.peticionesHTTP.get<User>(`${this.peticionesHTTP}/users?userName=${username}&password=${password}`);
+  }
+
+  updateUserLoggedStatus(id: string, status: boolean): Observable<User> {
+    return this.peticionesHTTP.patch<User>(`${this.peticionesHTTP}/users/${id}`, { estaLogueado: status });
+  }
 }
